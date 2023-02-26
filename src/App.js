510cf752eecfc41131643a9a011612bb1dc0fe59
ssingoudis <ssingoudis@gmail.com>
2023-02-26 03:20:29 +0100

    Lesson 62 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" }
+]
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() -0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setTurns(0)
+    setCards(shuffledCards)
+  }
+
+  const handleChoice = (card) => {
+    console.log(card)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './SingleCard.css'
+
+export default function SingleCard( {card, handleChoice} ) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
+  return (
+    <div className='card'>
+      <div>
+        <img 
+          className='front' 
+          src={card.src} 
+          alt="card front" 
+        />
+        <img 
+          className='back' 
+          onClick={handleClick} 
+          src='/img/cover.png' 
+          alt="card back" 
+        />
+      </div>
+    </div>
+    )}
