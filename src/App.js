801d2246b687169ef8202e988ff36abe5b98706c
ssingoudis <ssingoudis@gmail.com>
2023-03-02 14:13:07 +0100

    finished Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/sword-1.png" },
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" }
+  { "src": "/img/sword-1.png", matched: false },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false }
 ]
 
 function App() {
@@ -17,7 +17,11 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
+
+
+  //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() -0.5)
@@ -25,10 +29,47 @@ function App() {
 
     setTurns(0)
     setCards(shuffledCards)
+    setChoiceOne(null)
+    setChoiceTwo(null)
   }
 
+  // handle a choice
   const handleChoice = (card) => {
-    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect (() => {
+
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src && choiceOne.id !== choiceTwo.id) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // start game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
 
   return (
@@ -42,9 +83,12 @@ function App() {
             key={card.id} 
             card={card}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
